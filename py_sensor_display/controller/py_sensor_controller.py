f7cdb7a0390644afbd0137272c7b559c2c989ce0
TJ DeVries <timothydvrs1234@gmail.com>
2015-12-03 16:07:34 -0500

    Implement the other controller functions

--- py_sensor_display/controller/py_sensor_controller.py
+++ py_sensor_display/controller/py_sensor_controller.py
@@ -8,24 +8,25 @@ class Controller():
 	def add_machine(self, machine):
 		self.model.add_machine(machine)
 		
-	def remove_machine(self, machine):
-		pass
+	def remove_machine(self, id):
+		self.model.remove_machine(id)
 	
 	# Getters
 	def get_machines(self):
 		return self.model.get_machines()
 		
 	def get_machine_object(self, id):
-		pass
+		return self.model.get_machine_object(id)
 	
-	def get_machine_status(self, machine):
-		pass
+	def get_machine_status(self, id):
+		return self.model.get_machine_status(id)
+	
+	# Setters
+	def set_machine_status(self, id, status):
+		self.model.set_machine_status(id, status)
 		
-	def set_machine_status(self, machine, status):
-		pass
-		
-	def set_machine_location(self, machine, location):
-		pass
+	def set_machine_location(self, id, location):
+		self.model.set_machine_location(id, location)
 		
 	def get_model(self):
 		return self.model
