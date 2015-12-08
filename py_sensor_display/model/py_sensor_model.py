68b2a81b4fa5b245e1286aa15e2d09c185694f12
TJ DeVries <devries.timothyj@gmail.com>
2015-12-08 18:36:02 -0500

    Add support for the first print. IN COLOR!

--- py_sensor_display/model/py_sensor_model.py
+++ py_sensor_display/model/py_sensor_model.py
@@ -11,85 +11,119 @@ Made for Engineering 325,
 from enum import Enum
 
 class Model():
-	def __init__(self):
-		self.machines = {}
+    def __init__(self):
+        self.machines = {}
 
-	def add_machine(self, machine):
-		if machine.id in self.machines.keys():
-			raise Exception("Machine with this ID has already been added")
+    def add_machine(self, machine):
+        # Check if this ID has already been used.
+        if self.get_machine_object(machine.id):
+            raise Exception("Machine with this ID has already been added")
 
-		self.machines[machine.id] = machine
+        self.machines[machine.id] = machine
 
-	def remove_machine(self, id):
-		del machines[id]
+    def remove_machine(self, id):
+        del machines[id]
 
-	# Getters
+    # Getters
 
-	def get_machines(self):
-		return self.machines
+    def get_machines(self):
+        return self.machines
 
-	def get_machine_status(self, id):
-		return self.machines[id].get_status()
+    def get_machine_status(self, id):
+        return self.machines[id].get_status()
 
-	def get_machine_location(self, id):
-		return self.machines[id].get_location()
+    def get_machine_location(self, id):
+        return self.machines[id].get_location()
 
-	def get_machine_type(self, id):
-		return self.machines[id].get_type()
+    def get_machine_type(self, id):
+        return self.machines[id].get_type()
 
-	# Setters
+    # Setters
 
-	def set_machine_status(self, id, status):
-		self.machines[id].set_status(status)
+    def set_machine_status(self, id, status):
+        self.machines[id].set_status(status)
 
-	def set_machine_location(self, id, location):
-		self.machines[id].set_location(location)
+    def set_machine_location(self, id, location):
+        self.machines[id].set_location(location)
 
-	# maybe bad
-	def get_machine_object(self, id):
-		return self.machines[id]
+    # maybe bad
+    def get_machine_object(self, id):
+        if id in self.get_machines().keys():
+            return self.machines[id]
+        else:
+            return None
 
-	def get_machine_id(self, machine):
-		return
+    def get_machine_id(self, machine):
+        return
 
 class Status(Enum):
-	UNKNOWN = 0
-	OPEN = 1
-	BUSY = 2
-	RESERVED = 3
-	OUT_OF_ORDER = 4
+    UNKNOWN = 0
+    OPEN = 1
+    BUSY = 2
+    RESERVED = 3
+    OUT_OF_ORDER = 4
+
+    def color_string(self):
+        """
+        Returns the correct color start and finish sequence
+
+        :returns: A list where the first entry is the start of a color sequence,
+            and the second entry is the ending of the color sequence
+        """
+        color_start = '\033[00m'
+
+        if self.name == "OPEN":
+            color_start = '\033[32m'
+        elif self.name == "BUSY":
+            color_start = '\033[31m'
+
+        return [color_start, '\033[00m']
+
 
 class MachineType(Enum):
-	TREADMILL = 1
-	BICYCLE = 2
+    TREADMILL = 1
+    BICYCLE = 2
 
-	def __str__(self):
-		return self.name.capitalize()
+    def __str__(self):
+        return self.name.capitalize()
 
 
 class Machine():
-	def __init__(self, id, type, location):
-		self.id = id
-		self.type = type
-		self.location = location
-		self.status = Status.UNKNOWN
+    def __init__(self, id, type, location, color=False):
+        self.id = id
+        self.type = type
+        self.location = location
+        self.status = Status.UNKNOWN
 
-	# Getters
-	def get_id(self):
-		return self.id
+        # Our variable that holds whether we will print in color or not
+        self.color = color
 
-	def get_status(self):
-		return self.status
+    # Getters
+    def get_id(self):
+        return self.id
 
-	def get_location(self):
-		return self.location
+    def get_status(self):
+        return self.status
 
-	def get_type(self):
-		return self.type
+    def get_location(self):
+        return self.location
 
-	# Setters
-	def set_status(self, status):
-		self.status = status
+    def get_type(self):
+        return self.type
 
-	def set_location(self, location):
-		self.location = location
+    # Setters
+    def set_status(self, status):
+        self.status = status
+
+    def set_location(self, location):
+        self.location = location
+
+    # String representation
+    def __str__(self):
+        s = str(self.id)
+
+        if self.color:
+            status_color = self.get_status().color_string()
+            s = status_color[0] + s + status_color[1]
+
+        return s
--- py_sensor_display/view/py_sensor_view.py
+++ py_sensor_display/view/py_sensor_view.py
@@ -1,33 +1,146 @@
+from math import log10, ceil
+
 from model.py_sensor_model import Model
 #from gi.repository import Gtk
 
 class View():
-	def __init__(self, model):
-		self.model = model
-		# win = Gtk.Window()
-		# win.connect("delete-event", GTK.main_quit)
-		# win.show_all()
-		# GTK.main()
+    def __init__(self, model):
+        self.model = model
+        # win = Gtk.Window()
+        # win.connect("delete-event", GTK.main_quit)
+        # win.show_all()
+        # GTK.main()
 
-	def get_machines(self):
-		return self.model.get_machines()
-	
-	def get_machine_status(self, id):
-		return self.model.get_machine_status(id)
-		
-	def get_machine_location(self, id):
-		return self.model.get_machine_location(id)
-		
-	def get_machine_type(self, id):
-		return self.model.get_machine_type(id)
-	
-	def format_line(self, args):
-		return "| {0:3} | {1:15} |\n".format(args[0], args[1])
-		
-	def display_status(self):
-		final_string = self.format_line(["ID", "Machine Type"])
-		
-		for id in self.get_machines().keys():
-			final_string += self.format_line([id, self.get_machine_type(id)])
-			
-		return final_string
+    def get_machines(self):
+        return self.model.get_machines()
+
+    def get_machine_status(self, id):
+        return self.model.get_machine_status(id)
+
+    def get_machine_location(self, id):
+        return self.model.get_machine_location(id)
+
+    def get_machine_type(self, id):
+        return self.model.get_machine_type(id)
+
+    def format_line(self, args):
+        return "| {0:3} | {1:15} |\n".format(args[0], args[1])
+
+    def display_status(self):
+        final_list = self.format_line(["ID", "Machine Type"])
+
+        for id in self.get_machines().keys():
+            final_list += self.format_line([id, self.get_machine_type(id)])
+
+        return final_list
+
+    def display_locations(self):
+        """
+        Prints off a grid of the locations of current machines
+
+        :returns: A string with `\n` separated lines with the location of the each machine
+
+        """
+        final_list = list()
+
+        # TODO: Add in the third dimension handler eventually.
+        current_machines = self.get_machines()
+
+        current_locations = {}
+        for m in current_machines.keys():
+            current_locations[m] = current_machines[m].get_location()
+
+        x_max = None
+        x_min = None
+
+        y_max = None
+        y_min = None
+
+        machine_width = 0
+
+        for loc_ind in current_locations:
+            loc = current_locations[loc_ind]
+            if any(num is None for num in [x_max, x_min, y_max, y_min]):
+                x_min, x_max = loc[0], loc[0]
+                y_min, y_max = loc[1], loc[1]
+
+            if loc[0] > x_max:
+                x_max = loc[0]
+
+            if loc[0] < x_min:
+                x_min = loc[0]
+
+            if loc[1] > y_max:
+                y_max = loc[1]
+
+            if loc[1] < y_min:
+                y_min = loc[1]
+
+            temp_width = ceil(log10(loc_ind))
+            if  temp_width > machine_width:
+                machine_width = temp_width
+
+        if machine_width % 2 == 0:
+            machine_width += 1
+
+        surround_str = " "
+        border_str = '-' * (machine_width + 2 * len(surround_str))
+
+        num_side_char = 2
+        left_border = border_str[0:num_side_char] + (len(border_str) - num_side_char) * surround_str
+        right_border = left_border[::-1]
+
+        y_range = y_max + 2
+        x_range = x_max + 2
+        for y in range(0, y_range):
+            # Initialize the current row
+            current_row = []
+
+            # If y is the top or bottom row
+            if y in [0, y_range - 1]:
+                # Write a string of border_strings
+                for temp in range(0, x_range):
+                    current_row.append(border_str)
+            else:
+                for x in range(0, x_range):
+                    if x == 0:
+                        current_row.append(left_border)
+                    elif x == x_max + 1:
+                        current_row.append(right_border)
+                    else:
+                        # We have not yet found a match
+                        found = False
+                        for loc_ind, loc in current_locations.items():
+                            if [x, y] == loc[0:2]:
+                                # We found a match
+                                found = True
+
+                                # Handle colorful strings
+                                c_machine = current_machines[loc_ind]
+
+                                if c_machine.color:
+                                    additional = 10
+                                else:
+                                    additional = 0
+
+                                # Add the machine number in the picture
+                                current_row.append("{sur}{mac:^{width}}{sur}".format(sur=surround_str, mac=str(c_machine), width=machine_width + additional))
+                                break
+
+                        if not found:
+                            current_row.append(surround_str * (machine_width + 2 * len(surround_str)))
+
+            final_list.append(current_row)
+
+        # print([row for row in final_list])
+        # final_string = "\n".join(["{0}{1}{0}".format(surround_str, var) for row in final_list for var in row])
+
+        # TODO: One-liner
+        place_holder = []
+        for row in final_list:
+            temp = "".join(var for var in row)
+            place_holder.append(temp)
+
+        final_string = "\n".join(row for row in place_holder)
+
+        return final_string
