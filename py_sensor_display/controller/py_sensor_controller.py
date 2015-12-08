c544279f123087269363a9431880f9345e7663cb
TJ DeVries <devries.timothyj@gmail.com>
2015-12-08 18:35:46 -0500

    Retab the document

--- py_sensor_display/controller/py_sensor_controller.py
+++ py_sensor_display/controller/py_sensor_controller.py
@@ -10,32 +10,33 @@ Made for Engineering 325,
 from model.py_sensor_model import Model
 
 class Controller():
-	def __init__(self):
-		# Create our model
-		self.model = Model()
-		
-	def add_machine(self, machine):
-		self.model.add_machine(machine)
-		
-	def remove_machine(self, id):
-		self.model.remove_machine(id)
-	
-	# Getters
-	def get_machines(self):
-		return self.model.get_machines()
-		
-	def get_machine_object(self, id):
-		return self.model.get_machine_object(id)
-	
-	def get_machine_status(self, id):
-		return self.model.get_machine_status(id)
-	
-	# Setters
-	def set_machine_status(self, id, status):
-		self.model.set_machine_status(id, status)
-		
-	def set_machine_location(self, id, location):
-		self.model.set_machine_location(id, location)
-		
-	def get_model(self):
-		return self.model
+    def __init__(self):
+        # Create our model
+        self.model = Model()
+
+    def add_machine(self, machine):
+        # Add the machine to our model
+        self.model.add_machine(machine)
+
+    def remove_machine(self, id):
+        self.model.remove_machine(id)
+
+    # Getters
+    def get_machines(self):
+        return self.model.get_machines()
+
+    def get_machine_object(self, id):
+        return self.model.get_machine_object(id)
+
+    def get_machine_status(self, id):
+        return self.model.get_machine_status(id)
+
+    # Setters
+    def set_machine_status(self, id, status):
+        self.model.set_machine_status(id, status)
+
+    def set_machine_location(self, id, location):
+        self.model.set_machine_location(id, location)
+
+    def get_model(self):
+        return self.model
